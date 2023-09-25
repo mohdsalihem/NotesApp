1ef4de222fbc833be3ea192d7a36ce6a136017eb
Mohammed Salih <mohdsalihem@gmail.com>
2023-09-25 11:47:28 +0530

    removed unused imports

--- ClientApp/src/app.config.ts
+++ ClientApp/src/app.config.ts
@@ -2,12 +2,7 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {
-  HTTP_INTERCEPTORS,
-  provideHttpClient,
-  withInterceptors,
-  withInterceptorsFromDi,
-} from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { JwtInterceptor } from './app/interceptor/jwt.interceptor';
 
 export const appConfig: ApplicationConfig = {
